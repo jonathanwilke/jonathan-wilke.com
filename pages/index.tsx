a1b73fa37412b7cfa2d8917cf547cc6dd65e5ae9
Jonathan Wilke <info@jonathan-wilke.de>
2020-04-18 23:48:18 +0200

    added projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import Head from 'next/head'
+import projects from '../projects'
 
 export default function Home() {
-  const [isDarkMode, setDarkMode] = useState(true)
+  const currentHours = new Date().getHours()
+  const [isDarkMode, setDarkMode] = useState(
+    false // currentHours > 20 || currentHours < 8 ? true : false
+  )
 
   const getModeTextColor = () => {
     return isDarkMode ? 'text-white' : 'text-blue-900'
@@ -26,7 +30,7 @@ export default function Home() {
             : 'bg-gradient-light text-blue-900'
         }`}
       >
-        <nav className="px-6 py-8">
+        <nav className="p-8">
           <div className="container mx-auto">
             <img
               src={isDarkMode ? '/logo-dark.png' : '/logo-light.png'}
@@ -35,18 +39,18 @@ export default function Home() {
             />
           </div>
         </nav>
-        <div className="px-6 py-12">
-          <div className="container mx-auto mb-8">
-            <div className="flex flex-col md:flex-row">
+        <div className="px-8 py-12 md:py-16 lg:py-24">
+          <div className="container mx-auto">
+            <div className="flex flex-col md:flex-row md:items-center">
               <div className="mb-8 md:mb-0 mr-10">
                 <h2
                   className={`${
                     isDarkMode ? 'text-white' : 'text-blue-900'
-                  } text-5xl lg:text-6xl leading-none font-bold mb-6`}
+                  } text-4xl sm:text-5xl lg:text-6xl leading-none font-bold mb-6`}
                 >
                   Development <br />& Design
                 </h2>
-                <p className="mb-4">
+                <p className="mb-6">
                   Ich entwickle und gestalte für Sie individuelle Lösungen für
                   Websites und mobile Applikationen.
                 </p>
@@ -69,31 +73,114 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="bg-gradient-blue px-8 py-12">
+      <div
+        className={`${
+          isDarkMode
+            ? 'bg-gradient-dark text-blue-200'
+            : 'bg-gradient-light text-blue-900'
+        } px-8 pt-12`}
+      >
         <div className="container mx-auto">
-          <h1 className="text-white font-bold text-5xl lg:text-6xl mb-6 leading-none">
-            Über mich
-          </h1>
-          <p className="text-blue-900">
-            Ich bin Jonathan Wilke und studiere derzeit an der
-            Computervisualistik und Design im 6. Semester. <br />
-            <br /> Zudem entwickle und gestalte ich seit über 10 Jahren
-            leidenschaftlich Websites und Apps, wodurch ich schon viel Erfahrung
-            mit in einem breiten Spektrum von Programmiersprachen sammeln
-            konnte.
-          </p>
+          <div className="flex flex-col-reverse md:flex-row md:items-center">
+            <div className="md:w-1/2">
+              <img src="/jonathan.png" alt="Jonathan Wilke" />
+            </div>
+            <div className="md:ml-10 pb-8 md:pb-12 md:w-1/2">
+              <h1
+                className={`${
+                  isDarkMode ? 'text-white' : 'text-blue-900'
+                } font-bold text-4xl sm:text-5xl lg:text-6xl mb-6 leading-none`}
+              >
+                Über mich
+              </h1>
+              <p>
+                Ich bin Jonathan Wilke und entwickle und gestalte ich seit über
+                13 Jahren leidenschaftlich Websites und Apps, wodurch ich schon
+                viel Erfahrung mit in einem breiten Spektrum von
+                Programmiersprachen sammeln konnte.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
-      <footer>
-        <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-        </a>
-      </footer>
+      <div
+        className={`${
+          isDarkMode
+            ? 'bg-gradient-dark text-blue-200'
+            : 'bg-gradient-light text-blue-900'
+        } px-8 py-12`}
+      >
+        <div className="container mx-auto">
+          <h2
+            className={`${
+              isDarkMode ? 'text-white' : 'text-blue-900'
+            } text-4xl sm:text-5xl lg:text-6xl leading-none font-bold mb-6`}
+          >
+            Meine Projekte
+          </h2>
+          {projects.map((project, i) => (
+            <div
+              key={i}
+              className={`rounded-lg px-8 py-10 flex flex-col ${
+                i % 2 == 0 ? 'md:flex-row' : 'md:flex-row-reverse'
+              } items-center mb-6 text-center`}
+              style={{ background: project.backgroundColor }}
+            >
+              <div
+                className={`md:w-1/2 lg:w-1/3 mb-8 md:mb-0 ${
+                  i % 2 == 0 ? 'md:mr-12' : 'md:ml-12'
+                }`}
+              >
+                <img
+                  className="inline-block mb-6 w-full"
+                  style={{ maxWidth: '250px' }}
+                  src={project.logo}
+                  alt={project.title}
+                />
+                {project.description && (
+                  <p
+                    className={`${
+                      project.dark ? 'text-white' : 'text-blue-900'
+                    } mb-6`}
+                  >
+                    {project.description}
+                  </p>
+                )}
+                <div className="mb-6">
+                  {project.tags !== undefined &&
+                    project.tags.map((t, i) => (
+                      <span
+                        className="inline-block px-2 uppercase text-sm tracking-wide font-bold"
+                        style={{ color: project.mainColor }}
+                        key={`project-tag-${project.title}-${i}`}
+                      >
+                        #{t}
+                      </span>
+                    ))}
+                </div>
+                {project.link !== undefined && (
+                  <a
+                    className="inline-block text-center rounded-full text-white px-6 py-2"
+                    style={{ backgroundColor: project.mainColor }}
+                    href={project.link}
+                    target="__blank"
+                  >
+                    Seite ansehen &rarr;
+                  </a>
+                )}
+              </div>
+              <div className="md:w-1/2  lg:w-2/3">
+                <img
+                  className="max-w-full"
+                  src={project.image}
+                  alt={project.title}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
